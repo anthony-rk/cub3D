a675c2b06407f8e26863e65ac4ba3d25b615c899
anthony-rk <anthonyrkowalski@gmail.com>
2021-06-10 16:18:03 -0700

    Remove unneeded functions for Cub3D from libft

--- cub3D-Linux/src/libft/libft.h
+++ cub3D-Linux/src/libft/libft.h
@@ -50,8 +50,6 @@ char				*ft_strdup(const char *s1);
 char				*ft_substr(char const *s, unsigned int start, size_t len);
 char				*ft_strjoin(char const *s1, char const *s2);
 char				*ft_strtrim(char const *s1, char const *set);
-char				**ft_split(char const *s, char c);
-char				*ft_itoa(int n);
 char				*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void				ft_putchar_fd(char c, int fd);
 void				ft_putstr_fd(char *s, int fd);
@@ -65,8 +63,6 @@ void				ft_lstadd_back(t_list **lst, t_list *new);
 void				ft_lstdelone(t_list *lst, void (*del)(void *));
 void				ft_lstclear(t_list **lst, void (*del)(void *));
 void				ft_lstiter(t_list *lst, void (*f)(void *));
-t_list				*ft_lstmap(t_list *lst, \
-	void *(*f)(void *), void (*del)(void *));
 
 // Newly added functions
 char				*ft_itoa_base(int n, int base, int uppercase);
@@ -75,7 +71,6 @@ int					ft_nbrlen(long long n, int base);
 char				*ft_hextoa(unsigned long long n);
 char				*ft_hextoa_p(unsigned long long n);
 char				*ft_ltoa_base(long n, int base, int uppercase);
-char				*ft_itoa_unsigned(long int n);
 void				ft_putchar(char c);
 void				ft_putnchar(char c, int n);
 char				*ft_strnew(size_t size);
